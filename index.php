a427468668c36e208ceb112cd7bab74e38d4c4c3
Seteeng <andre.mendes@riogrande.rs.gov.br>
2020-02-17 10:27:28 -0300

    criamos a classe usuário

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
 
-$usuarios = $sql->query("SELECT * FROM tb_usuarios");
+$root->loadById(1);
 
-echo json_encode($usuarios);
+echo $root;
 
 ?>
